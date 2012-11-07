510254d3285312eda1484ef17da53a95c567f5af
vadia007 <vaaadik@mail.ru>
2012-11-07 02:07:55 +0200

    first commit

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2012-11-06 23:58:20 UTC
+#Software: Joomla Platform 12.2.0 Stable [ Neil Armstrong ] 21-September-2012 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2012-11-06T23:58:20+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
